35035ffda29cf80362b11c310d2d8aded5ce6d2b
DUVAN ANDRES VILLADIEGO HOYOS <davilladiego@nexos-software.com.co>
2024-01-29 16:09:14 -0500

    [FEAT] prime ng && angular interface added

--- Library.TESTS/UnitTest1.cs
+++ Library.TESTS/UnitTest1.cs
@@ -5,9 +5,10 @@ namespace Library.TESTS
         [SetUp]
         public void Setup()
         {
+
         }
 
-        [Test]
+        [Test] 
         public void Test1()
         {
             Assert.Pass();
